f683e547c6c8e7c5663d8e69e6d6d798ea74305d
Divyansh Yadav <divirao.05@gmail.com>
2023-08-24 21:19:25 +0530

    upload

--- cohort/cohort/settings.py
+++ cohort/cohort/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-dod%ycku!%mdg)u1qm&@g!q36zx$xznu^qxx!j=uvv!#^7@yq9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,6 +81,17 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'URL': os.getenv('postgresql://postgres:ejSBUM7fgiZFF9FISL1B@containers-us-west-107.railway.app:6487/railway'),
+#         'NAME': os.getenv('railway'),
+#         'USER': os.getenv('postgres'),
+#         'PASSWORD': os.getenv('ejSBUM7fgiZFF9FISL1B'),
+#         'HOST': os.getenv('containers-us-west-107.railway.app'),
+#         'PORT': os.getenv('6487'),
+#     }
+# }
 
 
 # Password validation
